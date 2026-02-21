991eb6bca89d031e34296bf561b9b1f3c1b66348
kts5927 <154499036+kts5927@users.noreply.github.com>
2026-02-21 14:48:11 +0900

    fix : cors url을 local -> env로 변경

--- src/main/java/com/projectsugarglider/config/CorsConfig.java
+++ src/main/java/com/projectsugarglider/config/CorsConfig.java
@@ -1,16 +1,21 @@
 package com.projectsugarglider.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    
+    @Value("${CORS_ALLOWED_ORIGINS}")
+    private String corsAllowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(corsAllowedOrigins)
                 .allowedMethods("GET", "POST", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
