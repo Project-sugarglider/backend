2e979638de7ab1f94658566416cd09ff7ece32fe
kts5927 <154499036+kts5927@users.noreply.github.com>
2026-03-09 04:13:57 +0900

    fix : 누락된 어노테이션 추가

--- src/main/java/com/projectsugarglider/front/controller/frontController.java
+++ src/main/java/com/projectsugarglider/front/controller/frontController.java
@@ -34,6 +34,7 @@ public class frontController{
     public record RegionPickRequest (String upper,  String lower) {}
     public record EntpReq(String entpId) {}
 
+    @ResponseBody
     @GetMapping("/")
     public String home() {
         return "ok";
